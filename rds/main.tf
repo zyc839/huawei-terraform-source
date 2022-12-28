986c233c5066ebb2f25bcd4f6d17218c322310e5
jiaojixiao <jixiao.jiao@daimler.com>
2022-12-28 09:24:20 +0800

    fix

--- rds/main.tf
+++ rds/main.tf
@@ -31,29 +31,59 @@ data "huaweicloud_rds_flavors" "flavor" {
   instance_mode = "ha"
 }
 
+
 resource "huaweicloud_rds_instance" "instance" {
-  name                = var.rds_instance_name
-  flavor              = data.huaweicloud_rds_flavors.flavor.flavors[0].id
-  ha_replication_mode = var.ha_replication_mode
-  vpc_id              = var.vpc_id != "default" ? var.vpc_id : data.huaweicloud_vpcs.vpc.vpcs[0].id
-  subnet_id           = var.subnet_id != "default" ? var.subnet_id : data.huaweicloud_vpc_subnets.subnet.subnets[0].id
+  name                = "terraform_test_rds_instance"
+  flavor              = "rds.pg.n1.large.2.ha"
+  ha_replication_mode = "async"
+  vpc_id              = "67ef1c7d-cc9d-4f50-8f30-58d9150ea5ee"
+  subnet_id           = "eb4247f1-6257-4de0-a264-36dd82d3f989"
   security_group_id   = huaweicloud_networking_secgroup.secgroup.id
   availability_zone   = [
-    var.primary_availability_zone,
-    var.standby_availability_zone
+    "cn-east-3b",
+    "cn-east-3a"
   ]
 
   db {
-    type     = var.db_type
-    version  = var.db_version
-    password = var.db_password
+    type     = "PostgreSQL"
+    version  = "12"
+    password = "Huangwei!120521"
   }
   volume {
-    type = var.volume_type
-    size = var.volume_size
+    type = "ULTRAHIGH"
+    size = 100
   }
   backup_strategy {
-    start_time = var.backup_strategy_start_time
-    keep_days  = var.backup_strategy_keep_days
+    start_time = "08:00-09:00"
+    keep_days  = 1
   }
 }
+
+
+
+# resource "huaweicloud_rds_instance" "instance" {
+#   name                = var.rds_instance_name
+#   flavor              = data.huaweicloud_rds_flavors.flavor.flavors[0].id
+#   ha_replication_mode = var.ha_replication_mode
+#   vpc_id              = var.vpc_id != "default" ? var.vpc_id : data.huaweicloud_vpcs.vpc.vpcs[0].id
+#   subnet_id           = var.subnet_id != "default" ? var.subnet_id : data.huaweicloud_vpc_subnets.subnet.subnets[0].id
+#   security_group_id   = huaweicloud_networking_secgroup.secgroup.id
+#   availability_zone   = [
+#     var.primary_availability_zone,
+#     var.standby_availability_zone
+#   ]
+
+#   db {
+#     type     = var.db_type
+#     version  = var.db_version
+#     password = var.db_password
+#   }
+#   volume {
+#     type = var.volume_type
+#     size = var.volume_size
+#   }
+#   backup_strategy {
+#     start_time = var.backup_strategy_start_time
+#     keep_days  = var.backup_strategy_keep_days
+#   }
+# }
--- rds/outputs.tf
+++ rds/outputs.tf
@@ -7,3 +7,9 @@ output "public_ips" {
   value = huaweicloud_rds_instance.instance.public_ips
   description = "instance public_ips"
 }
+
+
+output "public_ips" {
+  value = data.huaweicloud_rds_flavors.flavor
+  description = "instance public_ips"
+}
