242b0716273940d01d32efa21f5d2705d499d16f
jiaojixiao <jixiao.jiao@daimler.com>
2023-01-11 16:24:52 +0800

    fix

--- elb/outputs.tf
+++ elb/outputs.tf
@@ -2,3 +2,13 @@ output "elb_eip_ip" {
   value = huaweicloud_elb_loadbalancer.loadbalance.ipv4_eip
   description = "Elb public ip"
 }
+
+output "subnet" {
+  value = data.huaweicloud_vpc_subnets.subnet
+}
+
+
+output "vpc" {
+  value = data.huaweicloud_vpcs.vpc
+}
+
