5151ecd3556e1ad6bddedcce544a9b4957658f5e
jiaojixiao <jixiao.jiao@daimler.com>
2023-03-20 10:10:05 +0800

    fix

--- ingress-controller/variables.tf
+++ ingress-controller/variables.tf
@@ -1,7 +1,7 @@
 variable "ingress_release_name" {
   type        = string
   description = "Name of helm release"
-  default     = "ingress-nginx"
+  default     = "ingress-nginx-controller"
 }
 variable "ingress_namespace" {
   type        = string
