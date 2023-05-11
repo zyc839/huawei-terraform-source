7673de7c92cbfa77b6fc657ae55f79bee4ec455b
zhangyuchuan <zyc839@163.com>
2023-05-11 14:31:37 +0800

    test variables list

--- cce-vpc-eip-mysql/variables.tf
+++ cce-vpc-eip-mysql/variables.tf
@@ -458,11 +458,15 @@ variable "rds_switch" {
 }
 
 variable "rds_options" {
-  type = object({
-    a = string                # a required attribute
-    b = optional(string)      # an optional attribute
-    c = optional(number, 127) # an optional attribute with default value
-  })
+  type = list(object({
+    name    = string
+    enabled = optional(bool, true)
+    website = optional(object({
+      index_document = optional(string, "index.html")
+      error_document = optional(string, "error.html")
+      routing_rules  = optional(string)
+    }), {})
+  }))
 }
 
 
