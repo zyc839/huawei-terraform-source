1e2bfebcc93bb627da972ee957b06d274cf59f62
zhangyuchuan <zyc839@163.com>
2023-05-15 11:22:36 +0800

    test variables list

--- cce-vpc-eip-mysql/variables.tf
+++ cce-vpc-eip-mysql/variables.tf
@@ -476,25 +476,6 @@ variable "rds_options" {
   }
 }
 
-variable "availability_zone_names" {
-  type    = list(string)
-  default = ["us-west-1a","us-west-2a"]
-}
-
-variable "docker_ports" {
-  type = list(object({
-    internal = number
-    external = number
-    protocol = string
-  }))
-  default = [
-    {
-      internal = 8300
-      external = 8300
-      protocol = "tcp"
-    }
-  ]
-}
 
 
 
